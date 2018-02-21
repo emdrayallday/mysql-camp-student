69497727f7933f684b5d308f41c7d40ee1df4b13
Mark Johnson <markjohnson4542@gmail.com>
2018-02-21 15:11:16 -0500

    2 more queries

--- student-exercises/johnson-select.sql
+++ student-exercises/johnson-select.sql
@@ -5,3 +5,7 @@ SELECT * FROM individual WHERE LastName = 'Jennings';
 SELECT ID, LASTNAME FROM individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID, LastName, BirthDate FROM individual WHERE Year(BirthDate) > 1940;
 SELECT * FROM individual WHERE ID IN (1,3,5,7,19);
+SELECT * FROM band;
+SELECT * FROM band WHERE genre = 'Alternative' and isTogether='0';
+SELECT ID, name FROM band WHERE ID ='4';
+
